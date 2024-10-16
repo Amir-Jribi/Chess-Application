cdf15009c0f2b8f1881687a051faa65eb8a64b3e
Amir Jribi <amir.jribi.56@gmail.com>
2024-10-16 23:01:29 +0100

    first commit

--- server/server.js
+++ server/server.js
@@ -1,55 +1,66 @@
 const WebSocket=require('ws');
-const { v4: uuidv4 } = require('uuid');
+// const { v4: uuidv4 } = require('uuid');
 const wss=new WebSocket.Server({port : 5000});
 let waitingPlayer = null;
+const match= new Map();
+// const color = new Map();
+// const c=0;
+
+// table 1: game
+// id of the game ,
+// move history
+// id of the player who is black
+// id of the player who is white (so that the board be at the side)
+
+// table 2: player
+// id of the player
+c=0;
+allow=[];
+
+
 
 // json.parse : json string to javascript object
 // json.stringfy : javascript object to json string 
 wss.on('connection',(ws)=>{
-    const id = uuidv4(); // Generate a unique ID
-    clients[id] = ws;
-    // console.log('A new client connected');
-    
-    // message to be sent to the client
-    // ws.send('welcome to the websocket server');
-
-    // message received from the client
-    let opponent=null;
-    let color ='white';
     ws.on('message',(message)=>{
         const data=JSON.parse(message);
         console.log(data.type);
         if (data.type=='find-game'){
             if (waitingPlayer!=null){
-                console.log('matching 2 players!')
-                // ws.send(JSON.stringify({type:'start-game',color:'black',opponent:'player 1'}));
-                // waitingPlayer.send(JSON.stringify({type:'start-game',color:'white',opponent:'player 2'}));
-                // set opponent for both players
-                opponent=waitingPlayer;
-                waitingPlayer.opponent=ws;
+                allow[c]='true';
+                match.set(ws,waitingPlayer);
+                match.set(waitingPlayer,ws);
+                // color.set(ws,'w');
+                // color.set(waitingPlayer,'b');
+                ws.send(JSON.stringify({type:'start-game',color:'white',allow:'true'}));
+                waitingPlayer.send(JSON.stringify({type:'start-game',color:'black',allow:'false'}));
                 waitingPlayer=null;
-                // send start-game message for both palayers
-                ws.send(JSON.stringify({type:'start-game',color:'black',turn:'false'}));
-                opponent.send(JSON.stringify({type:'start-game',color:'white',turn:'true'}));
-
+                c++;
             }
             else {
                 waitingPlayer=ws;
                 ws.send(JSON.stringify({type:'waiting'}));
-
             }
-        }else if (data.type =='move'){
-            // if a player makes a move , send it to his opponent
-            if (opponent){
-                opponent.send(JSON.stringify({type:'move',move:data.move}));
-            }
+              
         }
-    })
+        else if (data.type=='move'){
+            // console.log(data.move);
+            ws.send(JSON.stringify({type:'move',allow:'false'}));
+            broadcast(JSON.stringify({type:'move',move:data.move,allow:'true'}),ws);
+        }
+    });
+
+    function broadcast (message,sender){
+        wss.clients.forEach((client)=>{
+            if (client.readyState ===WebSocket.OPEN && client!==sender && match.get(sender)===client){
+                client.send(message);
+            }
+        });
+    }
     ws.on('error',(error)=>{
         console.log(error);
-    })
+    });
 
-    // the client has closed the connection
     ws.on('close',()=>{
         console.log('client disconnected');
     });
